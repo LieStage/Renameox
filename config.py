1e916458da3e3ba24179a4c1bb28706cb67793a3
LieStage <118051769+LieStage@users.noreply.github.com>
2023-04-22 09:29:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,13 +6,13 @@ API_ID = os.environ.get("API_ID", "4682685")
 
 API_HASH = os.environ.get("API_HASH", "3eba5d471162181b8a3f7f5c0a23c307")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "5553062054:AAEwtZ2XWJHF42qQc5Bi-5VkO5t2Si3YXc8") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5484278199:AAFui1dWdQOo2L9WL8s75RhcYOi0oSJY5CE") 
 
 FORCE_SUB = os.environ.get("FORCE_SUB", "movie_time_botonly") 
 
 DB_NAME = os.environ.get("DB_NAME","telegramfilesrename")     
 
-DB_URL = os.environ.get("DB_URL","mongodb+srv://rename:bot@cluster0.go6qtqs.mongodb.net/?retryWrites=true&w=majority")
+DB_URL = os.environ.get("DB_URL","mongodb+srv://filetwo:link@cluster0.o7fziur.mongodb.net/?retryWrites=true&w=majority")
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
